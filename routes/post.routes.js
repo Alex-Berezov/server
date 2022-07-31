4eedd3656303045ba2a27f4a9bbc9ca26113069d
Aleksey Berezov <a.berezov@leningrad.io>
2022-07-31 18:50:21 +0400

    Finished app commit

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express"
+import Router from "express"
 import Post from '../models/Post.js'
 
 const router = Router()
@@ -12,9 +12,37 @@ router.post('/add', async (req, res) => {
     await post.save()
     res.json(post)
   } catch (error) {
-    console.log('Add post error >>', error)
+    console.log('Add post error on server >>', error)
   }
 })
 
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (error) {
+    console.log('Get posts error on server >>', error)
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id)
+    res.json(post)
+  } catch (error) {
+    console.log('Get single post error on server >>', error)
+  }
+})
+
+router.post('/remove', async (req, res) => {
+  try {
+    const  { id } = req.body
+    await Post.findOneAndDelete(id)
+    res.json({ message: 'Post was deleted' })
+  } catch (error) {
+    console.log('Delete post error on server >>', error)
+  }
+})
 
 export default router
